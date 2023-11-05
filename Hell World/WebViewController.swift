6439fa9e2fe1ca0bf28638f0f1e9060f67bd5816
gyumin <qmean59@gmail.com>
2023-11-05 14:56:19 +0900

    commit

--- Hell World/WebViewController.swift
+++ Hell World/WebViewController.swift
@@ -11,15 +11,25 @@ import WebKit
 class WebViewController: UIViewController {
 
     @IBOutlet var Webview: WKWebView!
+    
+    let url = URL(string: "http://localhost:8080/payment")!
+    private var webView: WKWebView!/** 웹 뷰 */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "http://localhost:8080/payment")!
+        
+        let config = WKWebViewConfiguration()
+        let preferences = WKWebpagePreferences()
+
+        /** JavaScript 사용 설정 */
+        preferences.allowsContentJavaScript = true
+
+        config.defaultWebpagePreferences = preferences
+
+        Webview = WKWebView(frame: view.frame, configuration: config)
+        self.view.addSubview(Webview)
         let request = URLRequest(url: url)
         Webview.load(request)
         // Do any additional setup after loading the view.
     }
-    
-
-    
-
 }
